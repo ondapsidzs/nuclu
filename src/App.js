1cf5eef10247b2284a8c2ab9587bf02fc8f476c5
Hatdoggy <ondaprey@gmail.com>
2021-10-07 09:16:47 +0800

    first update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { useState } from "react";
 
 import Landing from "./pages/Landing";
